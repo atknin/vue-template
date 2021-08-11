6454012a682111b48f13457bd878f6c2fa0e63cb
atknin <atknini@yahoo.com>
2021-08-11 19:12:21 +0300

    e

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app id="dayspan" v-cloak>
+
+  </v-app>
+</template>
+
+<script>
+import Vue from 'vue';
+
+
+export default {
+
+  name: 'app',
+
+  data: () => ({
+   
+  }),
+
+  mounted()
+  {
+    window.app = this.$refs.app;
+
+    this.loadState();
+  },
+
+  methods:
+  {
+    
+  }
+}
+</script>
+
+<style>
+
+
+</style>
